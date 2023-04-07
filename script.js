18603950477b40825050c8e8f01a645239fea064
katerinanosova <katerinanosova@gmail.com>
2023-04-07 21:06:13 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function getSeason() {
     let monthNumber = prompt('Введите номер месяца');
     if (monthNumber === '12' || monthNumber === '1' || monthNumber === '2') {
-        alert('Это зима')
+        alert('Это зима');
+        confirm('Сыграть ещё раз?') ? getSeason() : alert('Жаль');
     }
     else if (monthNumber === '3' || monthNumber === '4' || monthNumber === '5') {
-        alert('Это Весна')
+        alert('Это Весна');
+        confirm('Сыграть ещё раз?') ? getSeason() : alert('Жаль');
     }
     else if (monthNumber === '6' || monthNumber === '7' || monthNumber === '8') {
-        alert('Это Лето')
+        alert('Это Лето');
+        confirm('Сыграть ещё раз?') ? getSeason() : alert('Жаль');
     }
     else if (monthNumber === '9' || monthNumber === '10' || monthNumber === '11') {
-        alert('Это Осень')
+        alert('Это Осень');
+        confirm('Сыграть ещё раз?') ? getSeason() : alert('Жаль');
     }
     else {
         confirm('Вы ввели неправильное значение. Попробовать еще раз?') ? getSeason() : alert('А зря!');
